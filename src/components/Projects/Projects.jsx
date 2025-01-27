2b5676e76fb6ab233e107babedee1cf0d5ed4b2d
channer44 <123785136+channer44@users.noreply.github.com>
2025-01-27 04:37:00 -0500

    Upload Project Section
    #Updated the source files for the project section.
    #Added PROJECTS to the index.js file.
    #Uploaded pictures used for the project section.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,50 @@
 import React from "react";
 import styles from "./Projects.module.css";
+import { PROJECTS } from "../../index.js";
+
+const ProjectCard = ({ title, description, technologies, image }) => (
+  <div className={styles.projectCard}>
+    <div className={styles.imageContainer}>
+      {image ? (
+        <img src={image} 
+          alt={title} 
+          className={styles.projectImage}
+        />
+      ) : (
+        <div className={styles.imageGradient}/>
+      )}
+    </div>
+    <div className={styles.projectInfo}>
+      <h3 className={styles.projectTitle}>{title}</h3>
+      <p className={styles.projectDescription}>{description}</p>
+      <div className={styles.techStack}>
+        {technologies.map((tech) => (
+          <span key={tech} className={styles.techStyle}>
+            {tech}
+          </span>
+        ))}
+      </div>
+    </div>
+  </div>
+);
 
 export const Projects = () => {
-
   return (
     <section className={styles.container}>
-      <div className = {styles.content}>
-        <h2 className = {styles.title}>Projects</h2>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Projects</h2>
+        <div className={styles.projectsGrid}>
+          {PROJECTS.map((project) => (
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              technologies={project.technologies}
+              image={project.image}
+            />
+          ))}
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import UF from '/assets/education/UF.png';
 import UCF from '/assets/education/UCF.png';
+import librarymanager from '/assets/projects/librarymanager.jpg';
+import sudoku from '/assets/projects/sudoku.jpg';
+import familytree from '/assets/projects/familytree.jpg';
+import portfolio from '/assets/projects/portfolio.jpg';
 
 export const DESCRIPTION = "I am a fourth-year student at the University of Florida. Currently \
 immersed in the dynamic world of full-stack programming, I am open to new opportunities to expand my \
@@ -28,3 +32,32 @@ export const EDUCATION = [
     image: UCF
   }
 ]
+
+export const PROJECTS = [
+  {
+    title: "Portfolio Website",
+    description: "A personal portfolio website showcasing projects, skills, and contact details. Features dynamic animations, modular components, \
+    and a responsive design optimized for all devices.",
+    technologies: ["React", "JavaScript", "HTML", "CSS"],
+    image: portfolio
+  },
+  {
+    title: "Library Manager",
+    description: "A library management system handling inventory and checkout with seamless frontend-backend communication and a dynamic database of over 1,000 items.",
+    technologies: ["JavaScript", "HTML", "CSS", "Python"],
+    image: librarymanager
+  },
+  {
+    title: "Family Tree",
+    description: "A program for managing family trees using a balanced search tree. Supports adding/removing members and features versatile traversal algorithms.",
+    technologies: ["C++"],
+    image: familytree
+  },
+  {
+    title: "Sudoku",
+    description: "A Sudoku app with dynamic puzzle generation, difficulty levels, and robust solving algorithms, showcasing advanced backend development.",
+    technologies: ["Python"],
+    image: sudoku
+  }
+];
+
