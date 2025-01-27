41d0a60145078f231d8d83b2a0aed03502d5e382
channer44 <123785136+channer44@users.noreply.github.com>
2025-01-27 02:17:52 -0500

    Remove Contact Navigation
    #Removed the contact button on the navigation bar due to it being redundant.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -55,9 +55,6 @@ export const Navbar = () => {
           <li>
             <a href = "#projects">Projects</a>
           </li>
-          <li>
-            <a href = "#contact">Contact</a>
-          </li>
         </ul>
       </div>
     </nav> 
